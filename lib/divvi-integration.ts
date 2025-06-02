fc5309371d7c68f13e8a06466538b696037044b1
Jeremiah Oyeniran Damilare <jerydan148@gmail.com>
2025-06-02 22:59:31 +0100

    Update divvi-integration.ts

--- lib/divvi-integration.ts
+++ lib/divvi-integration.ts
@@ -6,6 +6,7 @@ const DIVVI_CONSUMER_ADDRESS: `0x${string}` = "0xd59B83De618561c8FF4E98fC29a1b96
 // Providers are the addresses of the Rewards Campaigns you signed up for
 const DIVVI_PROVIDERS: `0x${string}`[] = [
   '0x0423189886d7966f0dd7e7d256898daeee625dca',
+  '0xc95876688026be9d6fa7a7c33328bd013effa2bb',
   '0x5f0a55fad9424ac99429f635dfb9bf20c3360ab8'
 ];
 

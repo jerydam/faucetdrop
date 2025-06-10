70449eab5fe23fbd9234395dace15efaaf605970
Jeremiah Oyeniran Damilare <jerydan148@gmail.com>
2025-06-10 21:07:23 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ const getErrorInfo = (error: unknown): { code?: string | number; message: string
 const NETWORKS = {
   celo: {
     chainId: 42220, // Celo Mainnet
-    contractAddress: "0x190266890eF80cb9479F41dC6616107632FAa980",
+    contractAddress: "0xDD74823C1D3eA2aC423A9c4eb77f710472bdC700",
     name: "Celo",
   },
   lisk: {

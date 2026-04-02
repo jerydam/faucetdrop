2a8b167f7c08678c98b41e1fec3f2dae2cc2968a
jerydam <jerydan148@gmail.com>
2026-04-02 02:15:33 +0100

    rethink

--- V2/components/Analyticsdashboard.tsx
+++ V2/components/Analyticsdashboard.tsx
@@ -75,7 +75,7 @@ export interface QuizItem {
 
 type Tab = "faucet" | "quest" | "quiz";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? "https://faucetdrops-indexer.onrender.com";
 
 const EMPTY_FAUCET: FaucetAnalytics = {
   totalFaucets: 0, totalDrops: 0, uniqueUsers: 0, avgDropPerUser: 0,

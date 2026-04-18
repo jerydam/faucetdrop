37893afd217a616485518dce0ce4f19c74e95d6f
jerydam <jerydan148@gmail.com>
2026-04-18 22:56:47 +0100

    interactive

--- V3/app/(product)/quest/page.tsx
+++ V3/app/(product)/quest/page.tsx
@@ -38,7 +38,7 @@ export default function QuestPage() {
                     {/* Replace with actual image */}
                     <div className="absolute inset-0 flex items-center justify-center text-gray-500">
                       <Image
-                        src="/questShot.png"
+                        src="/quest.png"
                         alt="Quest Platform"
                         width={1000}
                         height={1000}
